df28f73a2a586dbf8cc34446cb0a8623122cb251
Ryan Dittmer <rdittmer@fullsail.edu>
2012-01-12 21:01:24 -0500

    Added base story, variables, and the number function.

--- Dittmer_Ryan_assignment2.js
+++ Dittmer_Ryan_assignment2.js
@@ -1,6 +1,59 @@
 /*Ryan Dittmer  
   01/12/2012    
   Deliverable 2 
- Insert Descript  */
+  Playing a game  */
+ 
+ var friendReady;
+ var friendOnline;
+ var playerName;
+ var playerScore;
+ 
+ 
+ friendReady  = true;
+ friendOnline = true;
+ playerName   = [ "ryan","jason","moe" ];
+ playerScore  = [  0,    0,   0  ];
+ 
+ 
+ //procedure
+ console.log( "I want to play a First Person Shooter game online. \nI want to play with my friends, are they online? ")
+ 
+ if ( friendOnline == true && friendReady == true )
+ 	console.log( "My friends are online and we are going to play." );
+ else
+ 	console.log( "I am just going to play by myself." );
+ 	
+ 	
+ //number function
+ function getScore( playerNum )
+ {
+ 	var deathLimit;
+ 	var randomNum;
+ 	var numDeaths;
+ 	var numKills;
+ 	
+ 	deathLimit = 5;
+ 	randomNum  = 0;
+ 	numDeaths  = 0;
+ 	numKills   = 0;
+ 	
+ 	console.log( playerName[ playerNum ] + " has begun the match." );
+ 	while ( numDeaths < deathLimit )
+ 	{
+ 		randomNum = Math.random();
+ 		randomNum = Math.round( randomNum );
+ 		if ( randomNum == 0 )
+ 		{
+ 			console.log( playerName[ playerNum ] + " has died. " );
+ 			numDeaths = numDeaths + 1;
+ 		}
+ 		else
+ 		{
+ 			console.log( playerName[ playerNum ] + " has killed an enemy." );
+ 			numKills = numKills + 1;
+ 		}
+ 	}
+ 	return numKills;
+ }
  
  
